ff1676f9218835aa233331f4086b3975ba0815b4
DESKTOP-A2KOA03 <dominiknovosad1994@gmail.com>
2024-12-10 21:02:43 +0100

    Pridané novšie knižnice

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert'; // Ensure this import is present
-import 'package:intl/intl.dart';
+import 'package:intl/intl_browser.dart';
 
 void main() {
   runApp(const MyApp());
